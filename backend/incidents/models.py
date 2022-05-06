824bef495dc3549a9b31b4f8eda48516a703071e
Amanda <aclaas522@gmail.com>
2022-05-06 15:57:12 -0500

    Completed building Authentication, Vehicles, and Incidents Models and Apps

--- backend/incidents/models.py
+++ backend/incidents/models.py
@@ -1,13 +1,16 @@
-from tkinter import CASCADE
 from django.db import models
 from authentication.models import User
+from vehicles.models import Vehicle
 
 class Incident(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     license_plate = models.CharField(max_length=8)
     incident_type = models.CharField(max_length=20)
     incident_description = models.CharField(max_length=1000)
     approximate_date_and_time = models.DateTimeField()
     approximate_location = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
 
 # Create your models here.
--- backend/vehicles/views.py
+++ backend/vehicles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Vehicle
+from .models import Incident
+from .serializer import VehicleSerializer
+from authentication.models import User
+
 # Create your views here.
